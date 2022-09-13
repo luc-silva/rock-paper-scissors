bc2b26f51c5976f452b576fdac3c011d23396b12
Lucas Santos <lucas.silva1203@outlook.com>
2022-09-13 17:51:58 -0300

    Modify conditional logic
    Modify the structure behind the logic of playRound() function.
    Also remove the error handling that was commented
    inside checkPlayerChoice() function.

--- script/script.js
+++ script/script.js
@@ -8,49 +8,29 @@ function checkPlayerChoice(string){
     string = string.toLowerCase().split("")
     string[0] = string[0].toUpperCase()
     return string.join("")
-    // if(options.includes(choice)){
-    //     return choice
-    // } else {
-    //     return "Something went wrong"
-    // }
-
 }
 
 function playRound(playerSelection, computerSelection){
     console.log(`\nYour choice: ${playerSelection} \nComputer choice: ${computerSelection}`)
-    if(playerSelection == "Paper"){
-        if(computerSelection == "Rock"){
-            return "You won this round!"
-        } else if (computerSelection == "Scissors"){
-            return "You lose this time..."
-        } else {
-            return "Oh, it's a draw."
-        }
-    } else if (playerSelection == "Rock") {
-        if(computerSelection == "Rock"){
-            return "Oh, it's a draw."
-        } else if (computerSelection == "Scissors"){
-            return "You won this round!"
-        } else {
-            return "You lose this time..."
-        }
-    } else if(playerSelection == "Scissors"){
-        if(computerSelection == "Rock"){
-            return "You lose this time..."
-        } else if (computerSelection == "Scissors"){
-            return "Oh, it's a draw."
-        } else {
-            return "You won this round!"
-        }
-    } else{
-        return "Something went wrong... See if there's a typo"
+    if(playerSelection == computerSelection){
+        return "It's a draw"
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors" 
+    || playerSelection == "Paper" && computerSelection == "Rock" 
+    || playerSelection == "Scissors" && computerSelection == "Paper"){
+        return "You won this round!"
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock"
+    || playerSelection == "Rock" && computerSelection == "Paper" 
+    || playerSelection == "Papper" && computerSelection == "Scissors"){
+        return "You lose this one..."
+    } else {
+        return "Something went wrong"
     }
 }
 
 function game(){
     for(let i = 0; i < 5; i++){
-        playerChoice = prompt("Please, make your choice: (rock, Paper or Scissors) ")
-        console.log(playRound(checkPlayerChoice(playerChoice), getComputerChoice()))
+        playerChoice = prompt("Please, make your choice: (Rock, Paper or Scissors) ")
+        console.log(`Result: ${playRound(checkPlayerChoice(playerChoice), getComputerChoice())}`)
     }
 }
 
